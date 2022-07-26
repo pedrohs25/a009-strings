3996d3c84fa4c91e17bc68bf8e41bfea20a20304
pedrohs25 <pedroh.cec.silva@gmail.com>
2022-07-26 15:46:48 -0300

    atualizando os exercicios a009-strings

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,9 +1,14 @@
-//Crie a const para a frase aqui
+//a)
 const frase1 = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+
+//b)
 const frase2 = `${frase1.replaceAll ("verde", "rosa")} `
 const frase3 = `${frase2.replaceAll ("azul", "laranja")} `
-
-
 console.log(frase3);
 
+//c)
+console.log(`A nova string inclui a palavra "verde"? \n${frase3.includes("verde")}`);
+console.log(`A nova string inclui a palavra "laranja"? \n${frase3.includes ("laranja")}`);
+
+//Extra
 console.log(`Passando o final da frase para maiúscula ficará assim: \n${frase3.replaceAll ("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")}`);
