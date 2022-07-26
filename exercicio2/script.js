851dd2b7d9dcbae05fcfd95b7c4449ffb61094ff
pedrohs25 <pedroh.cec.silva@gmail.com>
2022-07-26 12:53:19 -0300

    feito o exercicio a009-strings

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const fraseSemEspacos = minhaString.trim();
+console.log(`A frase: ${minhaString} \nPossui ${minhaString.length} caracteres \nA frase ${fraseSemEspacos} \nPossui ${fraseSemEspacos.length} caracteres depois da remoção dos espaços.`);
+
+const fraseSubstituida = fraseSemEspacos.replace ("________", "sticioso")
+
+console.log(`Substituindo os traços, a frase ficará: \n${fraseSubstituida}`);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
 //Crie a const para a frase aqui
+const frase1 = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+const frase2 = `${frase1.replaceAll ("verde", "rosa")} `
+const frase3 = `${frase2.replaceAll ("azul", "laranja")} `
+
+
+console.log(frase3);
+
+console.log(`Passando o final da frase para maiúscula ficará assim: \n${frase3.replaceAll ("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")}`);
